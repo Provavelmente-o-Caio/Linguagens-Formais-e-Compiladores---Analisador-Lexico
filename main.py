e6e79619a6478afcd72fac56d2dc378b6e7a74cd
provavelmente-o-caio <caiofecardoso@hotmail.com>
2025-11-21 20:01:54 -0300

    Ajustando exemplos

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-from src.analizador_lexico import AnalizadorLexico
-
-
-def main():
-    analisador_lexico = AnalizadorLexico()
-
-    analisador_lexico.ler_entradas("tests/input_exemplo_1.txt")
-
-    assert analisador_lexico.definicoes == {
-        "id": "[a-zA-Z]([a-zA-Z] | [0-9])*",
-        "num": "[1-9]([0-9])* | 0",
-    }
-
+import sys
+from src.analisador_lexico import AnalisadorLexico
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) < 3:
+        print("Uso: python main.py <arquivo_definicoes> <arquivo_entrada> [arquivo_saida]")
+        sys.exit(1)
+
+    analisador = AnalisadorLexico()
+    analisador.ler_definicoes(sys.argv[1])
+    analisador.gerar_analizador()
+    analisador.visualizar_automato()
+
+    arquivo_saida = sys.argv[3] if len(sys.argv) > 3 else None
+    analisador.analisar(sys.argv[2], arquivo_saida)
